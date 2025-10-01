4def230ea1f0efbbfdaf978bc97eaa981533dbbf
Hengyang Li <64343397+LiHengyang1@users.noreply.github.com>
2025-10-01 14:11:59 +0800

    Delete MonoSLMCavity directory

--- MonoSLMCavity/Fx_Fresnel_MTP.m
+++
@@ -1,5 +0,0 @@
-function Uout = Fx_Fresnel_MTP(Uin, paras)
-Uout = paras.omegaX * (Uin .* paras.Fresnelcore) * paras.omegaX;
-Uout = Uout * paras.ratio / length(Uout);
-Uout = Uout .* paras.FresnelCompensate;
-end
--- MonoSLMCavity/Fx_Fresnel_MTP_bp.m
+++
@@ -1,5 +0,0 @@
-function uin_ = Fx_Fresnel_MTP_bp(uout_, paras)
-uout_ = uout_ .* conj(paras.FresnelCompensate);
-uout_ = uout_ * paras.ratio / length(uout_);
-uin_ = (paras.omegaX' * uout_ * paras.omegaX') .* conj(paras.Fresnelcore);
-end
